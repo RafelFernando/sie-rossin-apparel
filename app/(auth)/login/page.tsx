933edbbd4503a7e6f45aa755ea0a19359b445ac1
Rafel Fernando <rafelganteng21@gmail.com>
2026-06-19 09:08:13 +0700

    fix layout and ui

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -13,8 +13,9 @@ export default async function Login({ searchParams }: {
     const params = (await searchParams)?.error
 
     return (
-        <div className="p-6 space-y-4">
-            <h1 className="text-2xl font-bold text-gray-900">Sign in to your account</h1>
+        <div className="p-6 space-y-4 bg-white w-[400px]">
+            <h1 className="text-lg font-bold text-gray-900 flex justify-center">Rossin Apparel</h1>
+            <h1 className="text-xl font-bold text-gray-900 flex justify-center">Sign in to your account</h1>
 
             {params === "OAuthAccountNotLinked" && (
                 <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-100" role="alert">
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,27 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TopLoader from "nextjs-toploader";
-import Sidebar from "@/components/layout/sidebar/sidebar";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/lib/auth";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Dashboard SIE",
-  description: "Data Analyst ",
+  description: "Data Analyst",
 };
 
 export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   const session = await auth();
 
   return (
-
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="h-screen overflow-hidden">
+    <html lang="en">
+      <body>
         <SessionProvider session={session}>
           <TopLoader color="#2563eb" height={5} />
-
-          <div className="flex h-full">
-            <Sidebar />
-            <main className="flex-1 overflow-y-auto p-6">
-              {children}
-            </main>
-          </div>
+          {children}
         </SessionProvider>
       </body>
     </html>
